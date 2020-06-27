3c04c7b749bb68365e339930323d644728e87769
Ricardo Casilimas <rcasilimas@gmail.com>
2020-06-27 16:20:44 -0400

    Added sticky navigation, gallery click picture functionality, smoothed out details, and updated gallery pictures

---
+++ script.js
@@ -0,0 +1,82 @@
+
+// GALLERY MODAL CLICK FUNCTIONALITY
+
+
+var img1 = document.getElementById("gallery-pic1");
+var img2 = document.getElementById("gallery-pic2");
+var img3 = document.getElementById("gallery-pic3");
+var img4 = document.getElementById("gallery-pic4");
+var img5 = document.getElementById("gallery-pic5");
+var img6 = document.getElementById("gallery-pic6");
+var img7 = document.getElementById("gallery-pic7");
+var img8 = document.getElementById("gallery-pic8");
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img5.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img6.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img7.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  img8.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+modal.onclick = function() {
+    modal.style.display = "none";
+  }
+
+// STICKY HEADER 
+
+var waypoint = new Waypoint({
+    element: document.getElementById('testimonial'),
+    handler: function(direction) {
+        if (direction == 'down') {
+            document.getElementById('nav').classList.remove('main-nav')
+            document.getElementById('nav').classList.add('sticky-nav')
+        } else {
+            document.getElementById('nav').classList.remove('sticky-nav')
+            document.getElementById('nav').classList.add('main-nav')
+        }
+    },
+    offset: '90%'
+  })
